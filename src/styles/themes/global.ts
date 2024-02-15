cb03bb0da40f56d3e743fc5802902e99e973bf48
Jhon Cruz <jhonskaz@gmail.com>
2024-02-15 14:04:27 -0300

    Adiciona estilos a página home

--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
 body {
     background: ${(props) => props.theme['gray-900']};
     color: ${(propos) => propos.theme.white};
+    -webkit-font-smoothing: antialiased;
 }
 
 body, input, textarea, button {
